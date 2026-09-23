f9f1da40f668a447c224a73e1b343559fc177992
Strekalovskaya Dinara <kuragatan0@gmail.com>
2026-09-23 11:34:00 +0900

    Delete

--- Practice1/Task2.cs
+++
@@ -1,7 +0,0 @@
-// double kurs = 84.39;
-
-// Console.Write("В рублях: ");
-// double rubles = double.Parse(Console.ReadLine());
-
-// double dollars = rubles / kurs;
-// Console.WriteLine("В долларах: " + dollars);
--- Practice1/Task3.cs
+++
@@ -1,9 +0,0 @@
-// double sum = 0;
-// for (int i = 1; i <= 7; i++)
-// {
-//     Console.Write("Температура " + i + ": ");
-//     sum += double.Parse(Console.ReadLine());
-// }
-
-// double sredn = sum / 7;
-// Console.WriteLine("Средняя температура: " + sredn);
